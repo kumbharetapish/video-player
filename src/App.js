3c07e2759df36da97af050d074fb84840d3af069
kumbharetapish <kumbharetapish12@gmail.com>
2020-02-20 17:34:21 +0530

    set all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import Axios from "axios";
 import "./App.css";
+import Video from "./Video";
+import List from "./List";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videoId: true, listRes: [], VideoObj: {} };
+    this.getVideo(1);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    Axios.get("https://5dfb78410301690014b8fbe2.mockapi.io/playlist/list")
+      .then(res => {
+        console.log(res.data);
+
+        this.setState({ listRes: [...res.data] });
+      })
+      .catch(err => {
+        this.setState({ listRes: [{ title: "not fount" }] });
+      });
+  }
+
+  getVideo = id => {
+    Axios.get(
+      "https://5dfb78410301690014b8fbe2.mockapi.io/playlist/video/" + id
+    )
+      .then(res => {
+        this.setState({ VideoObj: { ...res.data } });
+      })
+      .catch(err => {
+        this.setState({ VideoObj: {} });
+      });
+  };
+
+  clickHandel = id => {
+    console.log(id);
+
+    this.getVideo(id);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Video obj={this.state.VideoObj} />
+        <List listData={this.state.listRes} clickHandel={this.clickHandel} />
+      </div>
+    );
+  }
 }
 
 export default App;
